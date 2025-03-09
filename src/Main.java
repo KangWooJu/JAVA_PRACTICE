f15ef4fc5ba2fa7784f148dc5bb97c53f854acec
KangWooJu <space1102@naver.com>
2025-03-09 14:58:41 +0900

    feat:2장 실전문제 코드 업로드

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,84 @@
+import java.util.*;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in); // 문제풀이용 Scanner 클래스
+
+        /* 챕터2_실전문제_2번 문제
+        int birthday = scanner.nextInt();
+        int year = birthday/10000;
+        int month = (birthday - ( year * 10000 ))/100;
+        int day = birthday - (( year * 10000 ) + ( month * 100));
+
+        System.out.println(year+"년 "+month+"월 "+day+"일");
+         */
+
+
+        /* 챕터2_실전문제_8번
+        System.out.println("연산 입력>>");
+        double first = scanner.nextDouble();
+        String op = scanner.next();
+        double second = scanner.nextDouble();
+        double result;
+
+        switch(op){
+            case "곱하기":
+                result = first * second;
+                System.out.println(first+" "+op+" "+second+"의 계산 결과는 "+result);
+                break;
+
+            case "나누기":
+                result = first / second;
+                System.out.println(first+" "+op+" "+second+"의 계산 결과는 "+result);
+                break;
+
+            case "더하기":
+                result = first + second;
+                System.out.println(first+" "+op+" "+second+"의 계산 결과는 "+result);
+                break;
+
+            case "빼기":
+                result = first - second;
+                System.out.println(first+" "+op+" "+second+"의 계산 결과는 "+result);
+                break;
+
+        }
+         */
+
+        /* 챕터2_실전문제_9번
+        int first_x = 10;
+        int first_y = 10;
+        int second_x = 200;
+        int second_y = 300;
+
+        System.out.println("점 (x,y)의 좌표 입력>>");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if(( x < second_x )&( x > first_x )){
+            if(( y < second_y )&( y > first_x )){
+                System.out.println("("+x+","+y+")"+"는 사각형 안에 있습니다.");
+            }
+
+            if((y > second_y)||(y<first_y)){
+                System.out.println("("+x+","+y+")"+"는 사각형 밖에 있습니다.");
+            }
+        }
+
+        if((x==first_x)||(x==second_x)){
+            if((y<=second_y)&(y>=first_y)){
+                System.out.println("("+x+","+y+")"+"는 사각형 선 상에 있습니다.");
+            }
+
+            if((y>second_y)||(y<first_y)){
+                System.out.println("("+x+","+y+")"+"는 사각형 밖에 있습니다.");
+            }
+        }
+         */
+
     }
 }
