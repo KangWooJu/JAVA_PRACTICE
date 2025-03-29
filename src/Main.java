02a8a08fa27da8a33379e2ef21e62ba6b7d1730b
KangWooJu <space1102@naver.com>
2025-03-29 09:23:58 +0900

    feat:6장 실전문제 업로드

--- src/Main.java
+++ src/Main.java
@@ -231,7 +231,9 @@ public class Main {
          */
 
 
-        String username;
+        System.out.println("문자열을 입력하세요. 빈 칸이 있어도 되고 영어 한글 모두 됩니다.");
+        String input = scanner.nextLine();
+
 
 
 
